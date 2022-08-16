70371d70aabacfa9bb9147c32858f74ceda4fe39
Archisman-Mridha <archismanmridha12345@gmail.com>
2022-08-16 11:40:06 +0530

    learning how to create a deployment using kubernetes go client

---
+++ create-deployment/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	appsV1 "k8s.io/api/apps/v1"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	podReplicas int32= 2
+)
+
+func main( ) {
+	var kubeconfigFileLocation string = os.Getenv("HOME") + "/.kube/config"
+
+	kubeConfig, error := clientcmd.BuildConfigFromFlags("", kubeconfigFileLocation)
+	panicFilter(error)
+
+	// client set consists of the clients prepared for all the kubernetes api group
+	clientSet, error := kubernetes.NewForConfig(kubeConfig)
+	panicFilter(error)
+
+	appsV1APIClient := clientSet.AppsV1( )
+
+	//* deployment definition
+	deploymentDefinition := &appsV1.Deployment {
+
+		ObjectMeta: metaV1.ObjectMeta {
+
+			Name: "demo-deployment",
+			Namespace: "microservices",
+		},
+
+		Spec: appsV1.DeploymentSpec {
+			Replicas: &podReplicas,
+
+			Selector: &metaV1.LabelSelector {
+				MatchLabels: map[string]string {
+					"app": "demo",
+				},
+			},
+
+			Template: coreV1.PodTemplateSpec {
+
+				ObjectMeta: metaV1.ObjectMeta {
+					Labels: map[string]string {
+						"app": "demo",
+					},
+				},
+
+				Spec: coreV1.PodSpec {
+					Containers: [ ]coreV1.Container {
+						{
+							Name:  "web",
+							Image: "nginx:1.12",
+							ImagePullPolicy: coreV1.PullIfNotPresent,
+							Ports: [ ]coreV1.ContainerPort{
+								{
+									Name: "http",
+									Protocol: coreV1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// create the deployment
+	createdDeployment, error := appsV1APIClient.Deployments("microservices").Create(context.Background( ), deploymentDefinition, metaV1.CreateOptions{ })
+	panicFilter(error)
+
+	log.Printf("created deployment %s \n", createdDeployment.ObjectMeta.Name)
+}
+
+func panicFilter(providedError error) {
+
+	if providedError != nil {
+		panic(providedError.Error( ))}
+}
